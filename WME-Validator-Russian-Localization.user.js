efd2c63d854e16fe4228874b131b4320003364f8
Sergey Kuznetsov <sergey@izhevsk.pro>
2016-02-08 02:03:06 +0400

    fix 132

--- WME-Validator-Russian-Localization.user.js
+++ WME-Validator-Russian-Localization.user.js
@@ -749,7 +749,7 @@ window.WME_Validator_Russia = {
   "132.enabled": true,
   "132.params": {
     "template": "${street}",
-    "regexp": "/(^|\\s)(мкр[.н]?|им|пос|д|р-д|ул|пр-д|пер|пл-дь|наб|пр|просп|пр-к?т|пл|б-р|бул|наб|ш|дор|ал|туп|алл|р)(\\.?|\\s|$)/i",
+    "regexp": "/(^|\\s)(мкр[.н]?|им|пос|д|р-д|ул|пр-д|пер|пл-дь|наб|пр|просп|пр-к?т|пл|б-р|бул|наб|ш|дор|ал|туп|алл|р)(\\.|\\s|$)/i",
     "titleEN": "Abbreviated street type",
     "problemEN": "Abbreviated street type might cause search problems",
     "solutionEN": "Replace abbreviated street type with complete one"
